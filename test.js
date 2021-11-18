4ed1e1f209549bcd6e8155843fdf8572a9f68d4c
Mukesh Dani <mukeshdani00@gmail.com>
2021-11-18 23:00:57 +0530

    basic code

---
+++ test.js
@@ -0,0 +1,32 @@
+console.log('Mukesh Dani');
+
+// Variables 
+// Var let const
+
+  var a = 2 ;
+ console.log(a);
+// Reasign possible 
+
+a= 5;
+console.log(a);
+
+
+let b = 'Mukesh';
+console.log(b);
+
+//also Rassign possible 
+
+b = 21;
+
+console.log(b);
+
+const c=2;
+
+console.log(c);
+
+//Rassign not possible 
+
+/*  Error
+c=1;
+console.log(c);
+*/
