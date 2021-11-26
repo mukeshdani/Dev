723f9fa154b8ee89fe36b6a386d104f9570a1e9a
Mukesh Dani <mukeshdani00@gmail.com>
2021-11-27 00:33:22 +0530

    CLI project

---
+++ miniProjects/CliResume.js
@@ -0,0 +1,56 @@
+var inquirer = require('inquirer');
+const cp = require('child_process')
+
+
+function displayDetails(){
+inquirer
+  .prompt([
+    {
+           type:'list',
+           name :'Selection',
+           choices:['About' , 'Skills' , 'Projects' , 'Academics']
+    }
+  ])
+  .then((choices) => {
+       if(choices.Selection =='About'){
+              console.log(`Hii my name is mukesh dani , I am from uttarakhand , currently i'm pursuing Btech`)
+              displayNext()
+       }
+
+       else if(choices.Selection=='Skills'){
+              console.log('JavaScript , Node , React , GoLang , Java ')
+              displayNext()
+       }
+
+       else if(choices.Selection=='Projects'){
+            cp.execSync('start chrome https://github.com/mukeshdani')
+            displayNext()
+       }
+
+       else if(choices.Selection=='Academics'){
+              cp.execSync('start chrome ')
+              displayNext()
+       }
+  })
+ 
+}
+
+displayDetails()
+
+
+function displayNext(){
+       var inquirer = require('inquirer');
+inquirer
+  .prompt([
+         { type:'list',
+       name :'GoBack',
+       choices:['back']
+         }
+  ])
+  .then((choice) => {
+    if(choice.GoBack = 'back'){
+           displayDetails()
+    }
+  })
+ 
+}
