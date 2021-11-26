3ba89dfa912a3c2325fc486d5b10c058e2e1b20a
Mukesh Dani <mukeshdani00@gmail.com>
2021-11-26 21:29:37 +0530

    complete path

---
+++ Node Modules/path.js
@@ -0,0 +1,16 @@
+
+
+const path = require( 'path')
+
+let ext = path.extname('E:\web development\FJP webdev\Dev\Node Modules\f1.txt')
+
+console.log(ext)
+
+
+let basename = path.basename('E:\\web development\\FJP webdev\\Dev\\Node Modules\\f1.txt')
+
+console.log(basename)
+
+
+console.log(__dirname)
+console.log(__filename)
