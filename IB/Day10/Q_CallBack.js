c67b78513cac8da4d72a68509732b5664d531b14
Mukesh Dani <mukeshdani00@gmail.com>
2021-12-26 23:43:14 +0530

    add todays work

---
+++ IB/Day10/Q_CallBack.js
@@ -0,0 +1,13 @@
+let arr = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    ""
+]
+
+// add a fn to all arrays which takes as input two callbacks
+// one for small string (<10 in length ) and the other for long strings
+// short string callback should do the following 
+// My name is Mukesh Dani . I am a student . 
