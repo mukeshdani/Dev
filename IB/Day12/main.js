977cc1fca3e5a6a7879bd3ad943f23894740b0eb
Mukesh Dani <mukeshdani00@gmail.com>
2021-12-30 23:18:15 +0530

    add todays work

--- IB/Day12/main.js
+++ IB/Day12/main.js
@@ -1,6 +1,7 @@
+
 (function(){
 
-    /*  let btn = document.querySelector("#myId");
+      /*  let btn = document.querySelector("#myId");
     let divContainer = document.querySelector("#container");
     let myTemplates = document.querySelector("#myTemplate");
 
@@ -12,24 +13,97 @@
 
         divFolder.innerHTML = fname;
         divContainer.appendChild(divFolder); */
-        
+
+
     let btnAddFolder = document.querySelector("#btnAddFolder");
     let divContainer = document.querySelector("#divContainer");
     let pageTemplates = document.querySelector("#pageTemplates");
+    let fid = 0;
+    let folders = [];
 
-    btnAddFolder.addEventListener("click", function(){
-        let fname = prompt("name set kro folder ka  ");
-        if(fname==null){
+    btnAddFolder.addEventListener("click", addFolder);
+
+    function addFolder(){
+        let fname = prompt("Folder name?");
+        if(!fname){
             return;
         }
-         // codey from krne k lia ye kia hai
-        let divFolderTemplate = pageTemplates.content.querySelector(".folder");
-        let divFolder = document.importNode(divFolderTemplate, true);// true nhi kia toh us div ke ander wale nhi aayega 
 
-        let divName = divFolder.querySelector("[purpose='name']");// jb khud ka attribute use krna hai toh esa type krte hai 
+        fid++;
+        addFolderInPage(fname, fid);
+
+        folders.push({
+            id: fid,
+            name: fname
+        });
+        persistFoldersToStorage();
+    }
+
+    function deleteFolder(){
+        let divFolder = this.parentNode;
+        let divName = divFolder.querySelector("[purpose='name']");
+
+        let flag = confirm("Do you want to delete the folder " + divName.innerHTML);
+        if(flag == true){
+            divContainer.removeChild(divFolder);
+            
+            let idx = folders.findIndex(f => f.id == parseInt(divFolder.getAttribute("fid")));
+            folders.splice(idx, 1);
+            persistFoldersToStorage();
+        }
+    }
+
+    function editFolder(){
+        // yha pe ab hame closure mai nhi milega kuch islia 
+        //this use kr rke hai this mai parent mai jake chcek kr rhe honge 
+        let divFolder = this.parentNode;
+        
+        let divName = divFolder.querySelector("[purpose='name']");
+
+        let fname = prompt("Enter the new folder name");
+        if(!fname){
+            return;
+        }
+
         divName.innerHTML = fname;
 
-        divContainer.appendChild(divFolder);
+        let folder = folders.find(f => f.id == parseInt(divFolder.getAttribute("fid")));
+        folder.name = fname;
+        persistFoldersToStorage();
+    }
 
-    });
+    function addFolderInPage(fname, fid){
+        let divFolderTemplate = pageTemplates.content.querySelector(".folder");
+        let divFolder = document.importNode(divFolderTemplate, true);
+
+        let divName = divFolder.querySelector("[purpose='name']");
+        divName.innerHTML = fname;
+        divFolder.setAttribute("fid", fid);
+
+        let spanDelete = divFolder.querySelector("span[action='delete']");
+        spanDelete.addEventListener("click", deleteFolder);
+
+        let spanEdit = divFolder.querySelector("span[action='edit']");
+        spanEdit.addEventListener("click", editFolder);
+
+        divContainer.appendChild(divFolder);
+    }
+
+    function persistFoldersToStorage(){
+        console.log(folders);
+        let fjson = JSON.stringify(folders);
+        localStorage.setItem("data", fjson);
+    }
+
+    function loadFoldersFromStorage(){
+        let fjson = localStorage.getItem("data");
+        if(!!fjson){
+            folders = JSON.parse(fjson);
+            folders.forEach(function(f){
+                addFolderInPage(f.name, f.id);
+            })
+        }
+    }
+
+    loadFoldersFromStorage();
 })();
