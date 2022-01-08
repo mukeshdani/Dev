7e0654db99f509a773292c59445a1cd7fd0487b0
Mukesh Dani <mukeshdani00@gmail.com>
2022-01-08 17:57:43 +0530

    connect button with js

--- IB/Day17 18 19/main.js
+++ IB/Day17 18 19/main.js
@@ -1,4 +1,18 @@
 (function(){
-   
+   let btnaddFolder = document.querySelector("#addFolder");
+   let btnaddTextFolder = document.querySelector("#addTextFolder");
+
+   btnaddFolder.addEventListener("click" , addFolder);
+   btnaddTextFolder.addEventListener("click", addTextFile);
+
+   function addFolder(){
+        let fname = prompt("Enter Folder name ");
+        console.log(fname);
+   }
+
+   function addTextFile(){
+       let tfname = prompt("Enter File Name ");
+       console.log(tfname);
+   }
 })();
 //IIFI --> for prevent nameSpace Collison
