043b57c7315275ccc5d6f2a92c55e183b27cb623
Mukesh Dani <mukeshdani00@gmail.com>
2022-01-08 19:48:34 +0530

    code

--- IB/Day17 18 19/main.js
+++ IB/Day17 18 19/main.js
@@ -1,18 +1,61 @@
 (function(){
    let btnaddFolder = document.querySelector("#addFolder");
    let btnaddTextFolder = document.querySelector("#addTextFolder");
+   let divbreadcrumb = document.querySelector("#breadcrumb");
+   let divContainer = document.querySelector("#container");
+   let templates = document.querySelector("#templates");
 
    btnaddFolder.addEventListener("click" , addFolder);
    btnaddTextFolder.addEventListener("click", addTextFile);
 
    function addFolder(){
-        let fname = prompt("Enter Folder name ");
-        console.log(fname);
+    let fname = prompt("Enter folder name");
+    let divFolderTemplate = templates.content.querySelector(".folder");
+    let divFolder = document.importNode(divFolderTemplate, true);
+
+        let divName = divFolder.querySelector("[purpose='name']");
+        let spanRename = divFolder.querySelector("[action='rename']");
+        let spanDelete = divFolder.querySelector("[action='delete']");
+        let spanView = divFolder.querySelector("[action='view']");
+   
+
+        spanRename.addEventListener("click", renameFolder);
+        spanDelete.addEventListener("click", deleatFolder);
+        spanView.addEventListener("click", viewFolder);
+           divName.innerHTML = fname;
+    
+    divContainer.appendChild(divFolder);
    }
 
    function addTextFile(){
        let tfname = prompt("Enter File Name ");
        console.log(tfname);
    }
+   function deleatFolder(){
+       
+            console.log("in deleat ");
+   }
+   function addTextFolder(){
+
+   }
+   function renameFolder(){
+    console.log("in rename ");
+   }
+   function renameTextFile(){
+
+   }
+   function viewFolder(){
+    console.log("in View ");
+   }
+   function viewTextFile(){
+
+   }
+   function saveToStorage(){
+
+   }
+   function loadFromStorage(){
+
+   }
+   loadFromStorage();
 })();
 //IIFI --> for prevent nameSpace Collison
