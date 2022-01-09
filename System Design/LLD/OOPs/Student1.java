e69465ab1ded304bc40931bff52c3478c37b2916
Mukesh Dani <mukeshdani00@gmail.com>
2022-01-09 19:38:35 +0530

    added System Design Code and Notes

---
+++ System Design/LLD/OOPs/Student1.java
@@ -0,0 +1,44 @@
+package OOPs;
+
+public class Student1 {
+    public static class Student {
+        int age;
+        String name;
+
+        void announceYourself(){
+            System.out.println(this.name + "[" + this.age + "]");
+        }
+    }
+
+    public static void swap(Student s1, Student s2){
+        Student temp = s1;
+        s1 = s2;
+        s2 = temp;
+    }
+
+    public static void main(String[] args){
+        Student s1;
+
+        s1 = new Student();
+        s1.age = 10;
+        s1.name = "A";
+
+        Student s2 = new Student();
+        s2.age = 20;
+        s2.name = "B";
+
+
+        s1.announceYourself();
+        s2.announceYourself();
+        swap(s1, s2);
+        s1.announceYourself();
+        s2.announceYourself();
+
+        // Student s3 = s2;
+        // s3.age = 30;
+        // s3.name = "C";
+        // s3.announceYourself();
+
+        // s2.announceYourself();
+    }
+}
---
+++ System Design/LLD/OOPs/Student4.java
@@ -0,0 +1,52 @@
+package OOPs;
+
+public class Student4 {
+    public static class Student {
+        int age;
+        String name;
+
+        void announceYourself(){
+            System.out.println(this.name + "[" + this.age + "]");
+        }
+    }
+
+    public static void swap(Student s1, Student s2){
+        int tage = s1.age;
+        s1.age = s2.age;
+        s2.age = tage;
+
+        s1 = new Student();
+
+        String tname = s1.name;
+        s1.name = s2.name;
+        s2.name = tname;
+
+        s2 = new Student();
+    }
+
+    public static void main(String[] args){
+        Student s1;
+
+        s1 = new Student();
+        s1.age = 10;
+        s1.name = "A";
+
+        Student s2 = new Student();
+        s2.age = 20;
+        s2.name = "B";
+
+
+        s1.announceYourself();
+        s2.announceYourself();
+        swap(s1, s2);
+        s1.announceYourself();
+        s2.announceYourself();
+
+        // Student s3 = s2;
+        // s3.age = 30;
+        // s3.name = "C";
+        // s3.announceYourself();
+
+        // s2.announceYourself();
+    }
+}
