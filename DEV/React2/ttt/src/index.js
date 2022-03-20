f08c22fd1901c8252faab38b4ff2264138c53833
Mukesh Dani <mukeshdani00@gmail.com>
2022-03-20 22:01:07 +0530

    add button and also validate and setState -> history , setNumber and Status

--- DEV/React2/ttt/src/index.js
+++ DEV/React2/ttt/src/index.js
@@ -1,12 +1,20 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import './index.css'
-
-
+import './index.css';
 
 class Board extends React.Component {
-    render(){
-        return(
+    handleBoxClick(i){
+        this.props.handlerForBoxClick(i);
+    }
+
+    renderSquare(i){
+        return (
+            <button onClick={() => this.handleBoxClick(i)}>{this.props.boxes[i] == null? "": this.props.boxes[i]}</button>
+        );
+    }
+
+    render() {
+        return (
             <div className='board'>
                 <div className='title'>
                     Tic Tac Toe
@@ -14,59 +22,111 @@ class Board extends React.Component {
                 <div className='content'>
                     <div className="ttt">
                         <div className='row'>
-                            <button></button>
-                            <button></button>
-                            <button></button>
+                            {this.renderSquare(0)}
+                            {this.renderSquare(1)}
+                            {this.renderSquare(2)}
                         </div>
                         <div className='row'>
-                            <button></button>
-                            <button></button>
-                            <button></button>
+                            {this.renderSquare(3)}
+                            {this.renderSquare(4)}
+                            {this.renderSquare(5)}
                         </div>
                         <div className='row'>
-                            <button></button>
-                            <button></button>
-                            <button></button>
+                            {this.renderSquare(6)}
+                            {this.renderSquare(7)}
+                            {this.renderSquare(8)}
                         </div>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
+class Display extends React.Component {
+    render() {
+        let gameTitle = null;
 
-class Display extends React.Component{
-    render(){
-        let gameStatus = 'Next Move For "X"';
-        return(
+        if(this.props.gameStatus != null){
+            gameTitle = this.props.gameStatus
+        } else {
+            if(this.props.stepNumber % 2 == 0){
+                gameTitle = "Next move for X";
+            } else {
+                gameTitle = "Next move for O";
+            }
+        }
+
+        let buttons = [];
+        for(let i = 0; i <= this.props.stepNumber; i++){
+            let button = null;
+
+            if(i == 0){
+                button = (<button>Go to Start</button>);
+            } else {
+                button = (<button>Go to step number {i}</button>);
+            }
+
+            buttons.push(button);
+        }
+
+
+        return (
             <div className='display'>
                 <div className='title'>
-                    {gameStatus}
+                    {gameTitle}
                 </div>
                 <div className='content'>
                     <div className='history'>
-                        <button>Go to start</button>
-
+                        {buttons}
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
-class App extends React.Component {
-    
-    render(){
-        
-        return(
+
+class TTT extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            history: [
+                [null, null, null, null, null, null, null, null, null],
+            ],
+            stepNumber: 0,
+            gameStatus: null
+        }
+    }
+
+    handleSquareClick(i){
+        let oldHistory = this.state.history.slice();
+        let lastStateOfSquares = oldHistory[oldHistory.length - 1].slice();
+
+        if(lastStateOfSquares[i] != null){
+            return;
+        }
+
+        lastStateOfSquares[i] = this.state.stepNumber % 2 == 0? 'X': 'O';
+        oldHistory.push(lastStateOfSquares);
+
+        this.setState({
+            history: oldHistory,
+            stepNumber: this.state.stepNumber + 1,
+            gameStatus: null
+        })
+    }
+
+    render() {
+        let squares = this.state.history[this.state.history.length - 1];
+
+        return (
             <>
-            <Board />
-            <Display />
+                <Board handlerForBoxClick={(i) => this.handleSquareClick(i)} boxes={squares}/>
+                <Display stepNumber={this.state.stepNumber} gameStatus={this.state.gameStatus}/>
             </>
-        )
+        );
     }
 }
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<TTT />, document.getElementById("root"))
