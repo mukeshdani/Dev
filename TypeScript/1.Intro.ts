5fc25c469ada01ea0f933193e558d29f4a45762d
Mukesh Dani <mukeshdani00@gmail.com>
2023-01-12 22:57:43 +0530

    TypeScript Intro

---
+++ TypeScript/1.Intro.ts
@@ -0,0 +1,48 @@
+/* 
+
+Ts is not a new programming Language 
+TypeScript is a typed superset of javaScript 
+Develped by Microsoft 
+
+*/
+
+
+/* 
+
+Benifits of TypeScript 
+Easy Code Mangement 
+Support ECMAscript 
+We can use data types , classes , interface etc 
+IDE Support  
+
+*/
+
+
+/* 
+
+Prerequietic for typescript 
+
+Node js and npm 
+A modern code editor 
+A basic idea abput javaScript 
+
+
+ */
+
+
+
+/* 
+Basic Syntax 
+Functions 
+Tuples 
+Intefaces 
+Type and varicbles 
+number String 
+Union 
+Objects 
+Operators 
+Arrays
+Class 
+Much more 
+
+ */
