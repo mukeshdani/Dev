72a5642fcf14dd4d796abdd90642b641eb5448be
Mukesh Dani <mukeshdani00@gmail.com>
2023-01-13 20:53:08 +0530

    basic

---
+++ TypeScript/app.js
@@ -0,0 +1,10 @@
+console.log("Mukesh Dani");
+
+const input1 = document.querySelector("#number1");
+const input2 = document.querySelector("#number2");
+const button = document.querySelector("#button");
+
+button.addEventListener('click' , ()=>{
+      console.log(input1.value);
+      console.log(input2.value);
+});
