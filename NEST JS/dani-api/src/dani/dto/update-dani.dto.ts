3ef2a52a62c9a51af6fd2ab9e0ef301cdc251c8f
Mukesh Dani <mukeshdani00@gmail.com>
2023-03-27 23:11:33 +0530

    nest

--- NEST JS/dani-api/src/dani/dto/update-dani.dto.ts
+++ NEST JS/dani-api/src/dani/dto/update-dani.dto.ts
@@ -1,5 +1,5 @@
 //import { PartialType } from '@nestjs/mapped-types';
-import { PartialType } from '@nestjs/swagger'
+import { PartialType } from '@nestjs/swagger';
 import { CreateDaniDto } from './create-dani.dto';
 
 export class UpdateDaniDto extends PartialType(CreateDaniDto) {}
