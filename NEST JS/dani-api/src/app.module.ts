e1e7636cf52eea319a57ffaa44a22d836d2bdff6
Mukesh Dani <mukeshdani00@gmail.com>
2023-03-27 23:12:53 +0530

    nest

--- NEST JS/dani-api/src/app.module.ts
+++ NEST JS/dani-api/src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NinjasModule } from './ninjas/ninjas.module';
-import { NinjaController } from './ninja/ninja.controller';
 import { DaniModule } from './dani/dani.module';
-import { NinjaController } from './ninja/ninja.controller';
 
 @Module({
   imports: [NinjasModule, DaniModule],
-  controllers: [AppController, NinjaController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
