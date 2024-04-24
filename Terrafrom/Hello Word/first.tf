702db31184bdfc8a66f0e9e1dcd4505958d3e874
mukeshdani <mukeshdani00@gmail.com>
2024-04-24 15:03:03 +0530

    Terraform TUT

---
+++ Terrafrom/Hello Word/first.tf
@@ -0,0 +1,11 @@
+// blick "lanle1" "lable2"{
+// Identifiers = expressions
+//}
+
+//comments  This is first comment 
+# This is another comment 
+
+//Output 
+output hello1{
+    value = "Hello World"
+}
