434c566bf37ddbe6a160fe4442f7354db264df06
mukeshdani <mukeshdani00@gmail.com>
2024-04-24 15:15:01 +0530

    multiple terraform file in same directory

---
+++ Terrafrom/2/hello.tf
@@ -0,0 +1,7 @@
+// File Destrucure 
+// Current working directory m jitne tf files hongi sbko read krega and run krega 
+
+
+output "firstoutputblock"{
+     value = "This is my first block "
+}
---
+++ Terrafrom/2/second.tf
@@ -0,0 +1,3 @@
+output "secondoutputblock"{
+    value=" This is my second block"
+}
