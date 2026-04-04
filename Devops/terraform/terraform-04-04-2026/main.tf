de2dfa839f887413e59841fbb506c0c33800ddf2
mukeshdani <mukesh.dani00@gmail.com>
2026-04-04 16:05:01 +0530

    added

--- Devops/terraform/terraform-04-04-2026/main.tf
+++ Devops/terraform/terraform-04-04-2026/main.tf
@@ -16,3 +16,17 @@ resource "azurerm_resource_group" "mkd-rg-india" {
   location = "East US"
 }
 
+resource "azurerm_storage_account" "mkd-stg-india" {
+  name                    = "mkdstgindia"
+    resource_group_name     = azurerm_resource_group.mkd-rg-india.name
+    location                = azurerm_resource_group.mkd-rg-india.location
+    account_tier            = "Standard"
+    account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "mkd-container" {
+  name                  = "mkd-files-container"
+  storage_account_name  = azurerm_storage_account.mkd-stg-india.name
+  container_access_type = "private"
+}
+
