8804dce60f8e9d31e73de631ae625024dbca2a3f
mukeshdani <mukeshdani00@gmail.com>
2026-05-17 09:04:58 +0530

    added

---
+++ Devops/terraform/terraform-17-05-2026/Variables/terraform.tfvars
@@ -0,0 +1,2 @@
+resource_group_location = "East US"
+resource_group_name = "rg-terraform-17-05-2026"
---
+++ Devops/terraform/terraform-17-05-2026/Variables/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string  
+}
+
+variable "resource_group_location" {
+  description = "The location of the resource group"
+  type        = string 
+}
