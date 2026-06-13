8239539c60171c0cba73977b1b805816d65dd8fe
mukeshdani <mukeshdani00@gmail.com>
2026-06-13 16:55:00 +0530

    added k8 production setup init.

--- Devops/Kubernetes/K8-13-06-2026/main.tf
+++ Devops/Kubernetes/K8-13-06-2026/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_kubernetes_cluster" "mukeshaks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_D2as_v5"
   }
 
   identity {
