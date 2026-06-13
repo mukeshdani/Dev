1821c1d3842a2097e2897709a80de6fd3b6c3cb9
mukeshdani <mukeshdani00@gmail.com>
2026-06-13 10:19:24 +0530

    aaded k8s

---
+++ Devops/Kubernetes/K8-13-06-2026/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "4.77.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "mukeshrg" {
+  name     = "pmd-k8-13-06-2026"
+  location = "Central India"
+}
+
+resource "azurerm_kubernetes_cluster" "mukeshaks" {
+  name                = "pmd-k8-13-06-2026"
+  location            = azurerm_resource_group.mukeshrg.location
+  resource_group_name = azurerm_resource_group.mukeshrg.name
+  dns_prefix         = "pmdk8"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  depends_on = [
+    azurerm_resource_group.mukeshrg
+  ]
+}
