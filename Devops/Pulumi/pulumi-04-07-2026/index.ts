6df7542f7c51d69dc6c878f609a9c6695633968e
mukeshdani <mukeshdani00@gmail.com>
2026-07-05 11:31:23 +0530

    Pumuni Added

---
+++ Devops/Pulumi/pulumi-04-07-2026/index.ts
@@ -0,0 +1,20 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from "@pulumi/azure-native/resources";
+import * as storage from "@pulumi/azure-native/storage";
+
+// Create an Azure Resource Group
+const resourceGroup = new resources.ResourceGroup("myfirstpulumi-rg", {
+    location: "East US",
+});
+
+// Create an Azure Storage Account
+const storageAccount = new storage.StorageAccount("sa", {
+    resourceGroupName: resourceGroup.name,
+    sku: {
+        name: storage.SkuName.Standard_LRS,
+    },
+    kind: storage.Kind.StorageV2,
+});
+
+// Export the storage account name
+export const storageAccountName = storageAccount.name;
